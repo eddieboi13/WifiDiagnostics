106544b870fd47c560701f4850bab06b216aa259
Edward Hawkson <ehawkson@exeter.edu>
2025-05-09 21:30:33 -0400

    Updated code and added all relevant files

---
+++ Networking_ToolsDocument.swift
@@ -0,0 +1,39 @@
+//
+//  Networking_ToolsDocument.swift
+//  Networking Tools
+//
+//  Created by Edward Hawkson on 2/11/25.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var exampleText: UTType {
+        UTType(importedAs: "com.example.plain-text")
+    }
+}
+
+struct Networking_ToolsDocument: FileDocument {
+    var text: String
+
+    init(text: String = "Hello, world!") {
+        self.text = text
+    }
+
+    static var readableContentTypes: [UTType] { [.exampleText] }
+
+    init(configuration: ReadConfiguration) throws {
+        guard let data = configuration.file.regularFileContents,
+              let string = String(data: data, encoding: .utf8)
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+        text = string
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let data = text.data(using: .utf8)!
+        return .init(regularFileWithContents: data)
+    }
+}
